d9a19dfaca3fcc437dc87776595da6d14e20ea38
Catalin <catalin.ciobirca@yahoo.com>
2019-01-17 16:46:30 +0200

    architecture changes (viewport vtk)...

--- viewportvtk.cpp
+++ viewportvtk.cpp
@@ -6,10 +6,10 @@
 #include <vtkSmartPointer.h>
 #include <vtkRenderer.h>
 #include <vtkGenericOpenGLRenderWindow.h>
-#include <vtkImageImport.h>
 #include <vtkImageActor.h>
 #include <vtkInteractorStyleImage.h>
 #include <vtkRenderWindowInteractor.h>
+#include <vtkImageData.h>
 
 class ViewportVTKWidget : public QVTKOpenGLWidget
 {
@@ -23,12 +23,13 @@ private:
     void addImage(const cv::Mat &cvImage);
 
     void initImporter();
+    void convertTovtkImageData(const cv::Mat& frame);
 
 private:
     vtkSmartPointer<vtkRenderer> m_renderer;
-    vtkSmartPointer<vtkImageImport> m_importer;
     vtkSmartPointer<vtkImageActor> m_actor;
     vtkSmartPointer<vtkInteractorStyleImage> m_interactorStyle;
+    vtkSmartPointer<vtkImageData> m_imageData;
 };
 
 ViewportVTKWidget::ViewportVTKWidget(QWidget *parent)
@@ -52,36 +53,88 @@ ViewportVTKWidget::~ViewportVTKWidget()
 
 void ViewportVTKWidget::addImage(const cv::Mat &cvImage)
 {
-    m_importer->SetWholeExtent(0, cvImage.cols - 1, 0, cvImage.rows - 1, 0, cvImage.channels() - 1);
-    m_importer->SetDataExtentToWholeExtent();
+    convertTovtkImageData(cvImage);
+    m_actor->SetInputData(m_imageData);
 
-    m_importer->SetNumberOfScalarComponents(cvImage.channels());
-    m_importer->SetImportVoidPointer(cvImage.data);
-    m_importer->Update();
-
-    //m_actor->SetInputData(m_importer->GetOutput());
     m_renderer->ResetCamera();
     this->GetRenderWindow()->Render();
 }
 
 void ViewportVTKWidget::initImporter()
 {
-    m_importer = vtkSmartPointer<vtkImageImport>::New();
-
-    m_importer->SetDataSpacing( 1, 1, 1 );
-    m_importer->SetDataOrigin( 0, 0, 0 );
-
-
-    m_importer->SetDataScalarTypeToUnsignedChar();
-
     m_actor = vtkSmartPointer<vtkImageActor>::New();
-    m_actor->SetInputData(m_importer->GetOutput());
+
+    m_imageData = vtkSmartPointer<vtkImageData>::New();
+    m_actor->SetInputData(m_imageData);
+
     m_renderer->AddActor(m_actor);
     m_renderer->ResetCamera();
 
     m_interactorStyle = vtkSmartPointer<vtkInteractorStyleImage>::New();
     this->GetRenderWindow()->GetInteractor()->SetInteractorStyle(m_interactorStyle);
 
+}
+
+void ViewportVTKWidget::convertTovtkImageData(const cv::Mat &frame)
+{
+    vtkSmartPointer<vtkImageData> retval = vtkSmartPointer<vtkImageData>::New();
+
+    retval->SetDimensions(frame.cols, frame.rows, 1);
+    retval->SetSpacing(1,1,1);
+
+    retval->AllocateScalars(VTK_UNSIGNED_CHAR, frame.channels());
+
+    unsigned char* dest = reinterpret_cast<unsigned char*> (retval->GetScalarPointer());
+    uchar* src = frame.data;
+    int N = frame.rows * frame.cols;
+
+    if (frame.channels() == 3)
+    {
+        if (frame.isContinuous())
+        {
+            // 3-channel continous colors
+            for (int i = 0; i < N; ++i)
+            {
+                *dest++ = src[2]; // R
+                *dest++ = src[1]; // G
+                *dest++ = src[0]; // B
+                src += 3;
+            }
+        }
+        else
+        {
+//			std::cout << "noncontinous conversion, rows=" << size[1] << std::endl;
+            for (int i=0; i < frame.rows; ++i)
+            {
+                 const uchar* src = frame.ptr<uchar>(i);
+                 for (int j=0; j < frame.cols; ++j)
+                 {
+                        *dest++ = src[2];
+                        *dest++ = src[1];
+                        *dest++ = src[0];
+                        src += 3;
+                 }
+            }
+        }
+//		colorFormat = "ARGB";
+    }
+    if (frame.channels() == 1)
+    {
+        if (!frame.isContinuous())
+        {
+            std::cout << "Error: Non-continous frame data." << std::endl;
+            return;
+        }
+
+        // BW camera
+        for (int i = 0; i < N; ++i)
+        {
+            *dest++ = *src++;
+        }
+//		colorFormat = "R";
+    }
+
+    m_imageData = retval;
 }
 
 ////////////////////////////////////////////////////////////////////////////////////////
