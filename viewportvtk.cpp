bb956042820f9b3237c221a6dd0884e19194605a
Catalin <catalin.ciobirca@yahoo.com>
2019-01-18 02:09:58 +0200

    corrections.

--- viewportvtk.cpp
+++ viewportvtk.cpp
@@ -28,7 +28,8 @@ private:
     void resizeImage(int width, int height);
 
     void init();
-    void convertTovtkImageData(const cv::Mat& frame);
+    void convertTovtkImageData1(const cv::Mat& cvImage);
+    void convertTovtkImageData2(const cv::Mat& cvImage);
 
 private:
     vtkSmartPointer<vtkRenderer> m_renderer;
@@ -58,7 +59,7 @@ ViewportVTKWidget::~ViewportVTKWidget()
 
 void ViewportVTKWidget::addImage(const cv::Mat &cvImage)
 {
-    convertTovtkImageData(cvImage);
+    convertTovtkImageData1(cvImage);
     m_actor->SetInputData(m_imageData);
 
     //m_renderer->ResetCamera();
@@ -83,42 +84,104 @@ void ViewportVTKWidget::init()
 
 }
 
-void ViewportVTKWidget::convertTovtkImageData(const cv::Mat &frame)
+void ViewportVTKWidget::convertTovtkImageData1(const cv::Mat &cvImage)
 {
-    m_imageData->SetDimensions(frame.cols, frame.rows, 1);
+    m_imageData->SetDimensions(cvImage.cols, cvImage.rows, 1);
     m_imageData->SetSpacing(1,1,1);
 
-    int channels = frame.channels();
+    int channels = cvImage.channels();
     m_imageData->AllocateScalars(VTK_UNSIGNED_CHAR, channels);
 
     if (channels == 3)
     {
-        for(int i = 0; i < frame.cols; ++i)
+        for(int i = 0; i < cvImage.cols; ++i)
         {
-            for(int j = 0; j < frame.rows; ++j)
+            for(int j = 0; j < cvImage.rows; ++j)
             {
-                int mj = frame.rows - 1 - j;
+                int mj = cvImage.rows - 1 - j;
                 unsigned char* d = reinterpret_cast<unsigned char*> (m_imageData->GetScalarPointer(i, j, 0));
-                *d = frame.at<cv::Vec3b>(mj, i)[2];
-                *(d + 1) = frame.at<cv::Vec3b>(mj, i)[1];
-                *(d + 2) = frame.at<cv::Vec3b>(mj, i)[0];
+                *d = cvImage.at<cv::Vec3b>(mj, i)[2];
+                *(d + 1) = cvImage.at<cv::Vec3b>(mj, i)[1];
+                *(d + 2) = cvImage.at<cv::Vec3b>(mj, i)[0];
             }
         }
     }
     if (channels == 1)
     {
-        for(int i = 0; i < frame.cols; ++i)
+        for(int i = 0; i < cvImage.cols; ++i)
         {
-            for(int j = 0; j < frame.rows; ++j)
+            for(int j = 0; j < cvImage.rows; ++j)
             {
-                int mj = frame.rows - 1 - j;
+                int mj = cvImage.rows - 1 - j;
                 unsigned char* d = reinterpret_cast<unsigned char*> (m_imageData->GetScalarPointer(i, j, 0));
-                *d = frame.at<uchar>(mj, i);
+                *d = cvImage.at<uchar>(mj, i);
             }
         }
     }
 
     m_imageData->Modified();
+}
+
+void ViewportVTKWidget::convertTovtkImageData2(const cv::Mat &cvImage)
+{
+    vtkSmartPointer<vtkImageData> retval = vtkSmartPointer<vtkImageData>::New();
+
+    retval->SetDimensions(cvImage.cols, cvImage.rows, 1);
+    retval->SetSpacing(1,1,1);
+
+    retval->AllocateScalars(VTK_UNSIGNED_CHAR, cvImage.channels());
+
+    unsigned char* dest = reinterpret_cast<unsigned char*> (retval->GetScalarPointer());
+    uchar* src = cvImage.data;
+    int N = cvImage.rows * cvImage.cols;
+
+    if (cvImage.channels() == 3)
+    {
+        if (cvImage.isContinuous())
+        {
+            // 3-channel continous colors
+            for (int i = 0; i < N; ++i)
+            {
+                *dest++ = src[2]; // R
+                *dest++ = src[1]; // G
+                *dest++ = src[0]; // B
+                src += 3;
+            }
+        }
+        else
+        {
+//			std::cout << "noncontinous conversion, rows=" << size[1] << std::endl;
+            for (int i=0; i < cvImage.rows; ++i)
+            {
+                 const uchar* src = cvImage.ptr<uchar>(i);
+                 for (int j=0; j < cvImage.cols; ++j)
+                 {
+                        *dest++ = src[2];
+                        *dest++ = src[1];
+                        *dest++ = src[0];
+                        src += 3;
+                 }
+            }
+        }
+//		colorFormat = "ARGB";
+    }
+    if (cvImage.channels() == 1)
+    {
+        if (!cvImage.isContinuous())
+        {
+            std::cout << "Error: Non-continous frame data." << std::endl;
+            return;
+        }
+
+        // BW camera
+        for (int i = 0; i < N; ++i)
+        {
+            *dest++ = *src++;
+        }
+//		colorFormat = "R";
+    }
+
+    m_imageData = retval;
 }
 
 void ViewportVTKWidget::resizeGL(int width, int height)
