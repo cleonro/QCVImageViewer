a5f7be53f794f0278d52a111959b84595074da58
Catalin <catalin.ciobirca@yahoo.com>
2019-01-29 18:31:16 +0200

    added filterbase.

---
+++ filterbase.h
@@ -0,0 +1,23 @@
+#ifndef FILTER_BASE_H
+#define FILTER_BASE_H
+
+#include <opencv2/core/mat.hpp>
+
+#include <QObject>
+
+class FilterBase : public QObject
+{
+    Q_OBJECT
+
+public:
+    FilterBase(QObject *parent = nullptr);
+    ~FilterBase();
+
+signals:
+    void imageReady(cv::Mat &image);
+
+private:
+
+};
+
+#endif //FILTER_BASE_H
